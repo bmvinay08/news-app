5ddcd7f5065ce49e867544db711ca70e91cb6f39
Vinay Kumar B M <vinay.kumar@atonarp.com>
2022-01-26 10:08:28 +0530

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
+    <div className="news-app">
       <Button>Test</Button>
     </div>
   );
