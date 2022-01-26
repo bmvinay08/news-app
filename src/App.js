75fcdb528ec077ca90ec45e1307f1830ea1823b6
Vinay Kumar B M <vinay.kumar@atonarp.com>
2022-01-26 12:02:09 +0530

    Added date to navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { Navbar, Container, Form, FormControl, Button } from "react-bootstrap";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/common.scss';
+import './styles/app.scss';
 
 function App() {
+    const getCurrentDate = () => {
+        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const date = new Date();
+        return `${days[date.getDay()]}, ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+    };
     return (
         <div className="news-app-container">
-            <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar bg="dark" variant="dark" expand="lg" className="top-navbar">
                 <Container fluid>
-                    <Navbar.Brand href="#">UK News</Navbar.Brand>
-                    <Form className="d-flex float-right">
+                    <Navbar.Brand href="#">
+                        <span className="bold">UK News</span>
+                        <small className="date">{getCurrentDate()}</small>
+                    </Navbar.Brand>
+                    <Form className="d-flex">
                         <FormControl
                             type="search"
                             placeholder="Search News"
