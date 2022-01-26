f482a8b8da0ed9738f0ba6130a958a9bd961d432
Vinay Kumar B M <vinay.kumar@atonarp.com>
2022-01-26 11:11:38 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { Navbar, Container, Form, FormControl, Button } from "react-bootstrap";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="news-app">
-      <Button>Test</Button>
-    </div>
-  );
+    return (
+        <div className="news-app-container">
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand href="#">UK News</Navbar.Brand>
+                    <Form className="d-flex float-right">
+                        <FormControl
+                            type="search"
+                            placeholder="Search News"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Container>
+            </Navbar>
+        </div>
+    );
 }
 
 export default App;
